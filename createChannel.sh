7116e6d31d157f8b43a67f67b0140f56d7c57d0f
Minkyu-Jeon <minkyujeon9@gmail.com>
2021-03-13 20:17:03 +0900

    init project

---
+++ createChannel.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/artifacts/channel/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/artifacts/channel/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/artifacts/channel/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/artifacts/channel/config/
+
+setGlobalsForPeer0Org1(){
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+setGlobalsForPeer1Org1(){
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:8051
+}
+
+setGlobalsForPeer0Org2(){
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+}
+
+setGlobalsForPeer1Org2(){
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:10051
+}
+
+createChannel(){
+    rm -rf ./channel-artifacts/*
+    setGlobalsForPeer0Org1
+    
+    peer channel create -o localhost:7050 -c $CHANNEL_NAME \
+    --ordererTLSHostnameOverride orderer.example.com \
+    -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
+    --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+}
+
+joinChannel(){
+    setGlobalsForPeer0Org1
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer1Org1
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer0Org2
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer1Org2
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+}
+
+updateAnchorPeers(){
+    setGlobalsForPeer0Org1
+    peer channel update -o localhost:7050 \
+    --ordererTLSHostnameOverride orderer.example.com \
+    -c $CHANNEL_NAME \
+    -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx \
+    --tls $CORE_PEER_TLS_ENABLED \
+    --cafile $ORDERER_CA
+    
+    setGlobalsForPeer0Org2
+    peer channel update -o localhost:7050 \
+    --ordererTLSHostnameOverride orderer.example.com \
+    -c $CHANNEL_NAME \
+    -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx \
+    --tls $CORE_PEER_TLS_ENABLED \
+    --cafile $ORDERER_CA
+    
+}
+
+if [[ $# -lt 1 ]] ; then
+  echo "CHANNEL_NAME is required"
+  exit 0
+else
+  CHANNEL_NAME=$1
+  shift
+fi
+
+createChannel
+# joinChannel
+# updateAnchorPeers
